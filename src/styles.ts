5cc7a5370081d9390a2e77a94d0384337ccb4102
Charles Biney <charles.biney@amalitech.org>
2023-09-27 13:14:40 +0000

    chore: convert px to rem and add github workflow

--- src/styles.ts
+++ src/styles.ts
@@ -1,9 +1,9 @@
 export const style = {
-  borderRadius: "10px",
+  borderRadius: "0.625rem",
   background: "#343536",
   color: "#fff",
-  backdropFilter: "blur(18px) saturate(200%)",
+  backdropFilter: "blur(1.125rem) saturate(200%)",
   backgroundColor: "rgba(17, 20, 24, 0.785)",
-  border: "1px solid rgba(255, 255, 255, 0.125)",
-  boxShadow: "0 4px 30px rgba(12, 13, 26, 0.198)",
+  border: "0.0625rem solid rgba(255, 255, 255, 0.125)",
+  boxShadow: "0 0.25rem 1.875rem rgba(12, 13, 26, 0.198)",
 };
