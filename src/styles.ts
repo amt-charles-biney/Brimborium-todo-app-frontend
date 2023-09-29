b5be8b0de2c66805006e8b4c085370b0b175cffa
Charles Biney <charles.biney@amalitech.org>
2023-09-29 11:14:53 +0000

    feat: add toast utility and styles

---
+++ src/styles.ts
@@ -0,0 +1,9 @@
+export const style = {
+  borderRadius: "0.625rem",
+  background: "#343536",
+  color: "#fff",
+  backdropFilter: "blur(1.125rem) saturate(200%)",
+  backgroundColor: "rgba(17, 20, 24, 0.785)",
+  border: "0.0625rem solid rgba(255, 255, 255, 0.125)",
+  boxShadow: "0 0.25rem 1.875rem rgba(12, 13, 26, 0.198)",
+};
