08c158b72b9bec64e9ea8a8e9faea05e0ce04546
amt-charles-biney <charles.biney@amalitech.com>
2023-10-05 15:26:30 +0000

    feat: change embed element

--- src/components/cards/spotify.tsx
+++ src/components/cards/spotify.tsx
@@ -1,7 +1,13 @@
 const Spotify = () => {
   return (
-    <div className="spotify h-full flex items-center justify-center">
-      <div id="embed-iframe"></div>
+    <div className="spotify h-full flex items-end justify-center">
+      <iframe
+        src="https://open.spotify.com/embed/album/260iH4R6MRNcDQ79106Icv?utm_source=generator"
+        width="104%"
+        height="160"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
     </div>
   );
 };
