18b709c653986109f96dc9119924aa3e9fdfed88
amt-charles-biney <charles.biney@amalitech.com>
2023-10-31 11:54:53 +0000

    feat: update redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,21 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
-import authSlice from "./authSlice";
+import rootReducer from "./rootReducer";
 
 const persistConfig = {
-    key: 'root',
-    storage,
-  }
-  
-  const persistedReducer = persistReducer(persistConfig, authSlice)
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: [thunk],
 });
 
-
-export const persister = persistStore(store)
+export const persister = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
