e6df5ba7311d147f65012b2ecf751ee2c3cef39f
amt-charles-biney <charles.biney@amalitech.com>
2023-11-22 08:58:54 +0000

    fix: remove React strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App.tsx";
 import "./index.css";
-import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
 );
