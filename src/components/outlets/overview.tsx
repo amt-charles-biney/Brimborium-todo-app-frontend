f86a33d27eeabdc4bd172b863170d291b5cee069
amt-charles-biney <charles.biney@amalitech.com>
2023-11-22 08:35:21 +0000

    feat: show weather and pomodoro components

--- src/components/outlets/overview.tsx
+++ src/components/outlets/overview.tsx
@@ -1,9 +1,11 @@
 import Card from "../card";
 import DigitalClock from "../cards/clock";
 import CompletionChart from "../cards/completionChart";
+import Pomodoro from "../cards/pomodoro";
 import Quote from "../cards/quote";
 import Spotify from "../cards/spotify";
 import UpcomingTask from "../cards/upcomingTask";
+import Weather from "../cards/weather";
 
 const Overview = () => {
   return (
@@ -12,9 +14,9 @@ const Overview = () => {
       <Card title={"Current task"} content={<UpcomingTask index={0} />}></Card>
       <Card title={"Completion Ratio"} content={<CompletionChart />}></Card>
       <Card title={"Motivational Quote"} content={<Quote />}></Card>
-      <Card title={"Weather"} content={undefined}></Card>
+      <Card title={"Weather"} content={<Weather />}></Card>
       <Card title={"Upcoming Task"} content={<UpcomingTask index={1} />}></Card>
-      <Card title={"Pomodoro Timer"} content={undefined}></Card>
+      <Card title={"Pomodoro Timer"} content={<Pomodoro />}></Card>
       <Card title={"Spotify Music"} content={<Spotify />} bg="#b2598582"></Card>
     </>
   );
