b09e7e84011e2674555bc5c177bf3d8d55840a83
amt-charles-biney <charles.biney@amalitech.com>
2023-11-22 08:53:45 +0000

    feat: make draggable and change behaviour of mouse events

--- src/components/qat.tsx
+++ src/components/qat.tsx
@@ -1,53 +1,75 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
+import Draggable from "react-draggable";
 import { qatButtons } from "../data/qat-buttons";
-import type { QatButtonProps, openModal } from "../models/ui";
+import type { QatButtonProps, OpenModal } from "../models/ui";
 import { QatButton } from "./qat-button";
 
-const Qat = ({ openModal }: openModal) => {
-  const [isQatOpen, setIsQatOpen] = useState(false);
+const Qat = ({ openModal }: OpenModal) => {
+  const [isQatOpen, setIsQatOpen] = useState<boolean>(false);
 
   const toggleQat = () => {
     const element = document.getElementById("qat_btn");
 
-    const angle = element!.style.transform;
-    if (angle !== "rotate(45deg)") {
-      element!.style.transform = "rotate(45deg)";
-    } else {
-      element!.style.transform = "rotate(0)";
+    if (!element) {
+      return;
     }
 
-    setIsQatOpen(!isQatOpen);
+    const rotationAngle = element.style.transform;
+    const isOpen = rotationAngle === "rotate(45deg)";
+
+    element.style.transform = isOpen ? "rotate(0)" : "rotate(45deg)";
+    setIsQatOpen(!isOpen);
+  };
+
+  const resetQat = () => {
+    const element = document.getElementById("qat_btn");
+
+    if (!element) {
+      return;
+    }
+
+    if (isQatOpen) {
+      setTimeout(() => {
+        element.style.transform = "rotate(0)";
+        setIsQatOpen(false);
+      }, 5000);
+    }
   };
 
   return (
-    <div className="fixed bottom-10 right-10 flex flex-col gap-2 items-end">
-      {isQatOpen && (
-        <div className="flex flex-col gap-2">
-          {qatButtons.map((qat: QatButtonProps) => {
-            return (
-              <QatButton
-                key={qat.title}
-                color={qat.color}
-                icon={qat.icon}
-                title={qat.title}
-                openModal={qat.component && openModal}
-                component={qat.component}
-              />
-            );
-          })}
-        </div>
-      )}
-
+    <Draggable>
       <div
-        id="qat_btn"
-        onClick={toggleQat}
-        className="w-[40px] h-[40px] rounded-full bg-green-500 cursor-pointer flex justify-center items-center transition"
+        onMouseLeave={resetQat}
+        className="fixed bottom-10 right-10 flex flex-col gap-2 items-end"
       >
-        <FontAwesomeIcon className="text-lg" icon={faPlus} />
+        {isQatOpen && (
+          <div className="flex flex-col gap-2">
+            {qatButtons.map((qat: QatButtonProps) => {
+              return (
+                <QatButton
+                  key={qat.title}
+                  color={qat.color}
+                  icon={qat.icon}
+                  title={qat.title}
+                  openModal={qat.component && openModal}
+                  component={qat.component}
+                />
+              );
+            })}
+          </div>
+        )}
+
+        <div
+          id="qat_btn"
+          onClick={toggleQat}
+          className="w-[40px] h-[40px] rounded-full bg-green-500 cursor-pointer flex justify-center items-center transition"
+        >
+          <FontAwesomeIcon className="text-lg" icon={faPlus} />
+        </div>
       </div>
-    </div>
+    </Draggable>
   );
 };
 
