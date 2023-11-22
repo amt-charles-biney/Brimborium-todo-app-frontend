1b7fcaa3365e2b702546e9f57715931a9ff7fc0c
amt-charles-biney <charles.biney@amalitech.com>
2023-11-22 08:38:57 +0000

    feat: add task sync feature to slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import thunk from "redux-thunk";
+import thunk, { ThunkDispatch } from "redux-thunk";
 import rootReducer from "./rootReducer";
+import { fetchUserTasks } from "./taskSlice";
 
 const persistConfig = {
   key: "root",
@@ -15,6 +16,23 @@ export const store = configureStore({
   reducer: persistedReducer,
   middleware: [thunk],
 });
+
+const sync = () => {
+  const state = store.getState();
+  const { isAuthenticated, user } = state.auth;
+
+  if (isAuthenticated && !!user) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (store.dispatch as ThunkDispatch<RootState, void, any>)(
+      fetchUserTasks(user.id as string)
+    );
+  }
+
+  const intervalInMilliseconds = 1 * 30 * 1000;
+  setTimeout(sync, intervalInMilliseconds);
+};
+
+sync();
 
 export const persister = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
