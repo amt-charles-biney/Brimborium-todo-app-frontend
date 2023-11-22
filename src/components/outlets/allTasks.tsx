ed5d1a31f0cfab0ecbf3e26ad671b3e2247d66c5
amt-charles-biney <charles.biney@amalitech.com>
2023-11-22 08:50:41 +0000

    refactor: use emptyListPrompt and tableHeaders components

--- src/components/outlets/allTasks.tsx
+++ src/components/outlets/allTasks.tsx
@@ -1,6 +1,6 @@
-import { faSort, faSortNumericAsc } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useAppSelector } from "../../redux/hooks";
+import EmptyListPrompt from "../emptyListPrompt";
+import TableHeaders from "../tableHeaders";
 import TaskItem from "../taskItem";
 
 const AllTasks = () => {
@@ -8,34 +8,22 @@ const AllTasks = () => {
 
   return (
     <div className="w-full h-full flex">
-      <div className="w-full min-w-[50.75rem] h-full">
-        <ul className="flex gap-10 px-5 font-bold text-lg pb-3 text-center">
-          <li className="w-[35%] text-left cursor-pointer">
-            Topic <FontAwesomeIcon className="text-xs" icon={faSort} />
-          </li>
-          <li className="w-[15%] cursor-pointer">
-            Deadline{" "}
-            <FontAwesomeIcon className="text-sm" icon={faSortNumericAsc} />
-          </li>
-          <li className="w-[10%] cursor-pointer">
-            Status <FontAwesomeIcon className="text-xs" icon={faSort} />
-          </li>
-          <li className="w-[15%] cursor-pointer">
-            Project <FontAwesomeIcon className="text-xs" icon={faSort} />
-          </li>
-          <li className="w-[15%] cursor-pointer">
-            Progress <FontAwesomeIcon className="text-xs" icon={faSort} />
-          </li>
-          <li className="w-[10%]">Assigned</li>
-        </ul>
-        <ul className="flex flex-col">
-          {tasks.map((task) => {
-            return <TaskItem task={task} key={task.id} />;
-          })}
-        </ul>
-        {/* TODO: Page numbers */}
-      </div>
-      {/* TODO: Add a side modal.*/}
+      {tasks.length < 1 ? (
+        <EmptyListPrompt
+          topic={"No tasks found."}
+          message={"Try adding some tasks to help manage your project."}
+        />
+      ) : (
+        <div className="w-full min-w-[50.75rem] h-full">
+          <TableHeaders />
+          <ul className="flex flex-col">
+            {tasks.map((task, index) => {
+              return <TaskItem index={index} task={task} key={task.id} />;
+            })}
+          </ul>
+          {/* TODO: Page numbers */}
+        </div>
+      )}
     </div>
   );
 };
