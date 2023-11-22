b25408ab4ade86bd876dd39b22f9eed50c224281
amt-charles-biney <charles.biney@amalitech.com>
2023-11-22 08:58:00 +0000

    feat: add more links

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,12 +1,48 @@
 import {
-  faCalendarCheck,
   faCalendarPlus,
   faChartPie,
   faCubesStacked,
   faListCheck,
+  faProjectDiagram,
+  faShield,
   faSliders,
+  faUser,
+  faUsersRectangle,
 } from "@fortawesome/free-solid-svg-icons";
 import AddTask from "../components/addTask";
+
+export const taskLinks = [
+  {
+    link: "/",
+    icon: faCubesStacked,
+    name: "Dashboard",
+    modal: false,
+  },
+  {
+    icon: faCalendarPlus,
+    name: "New task",
+    modal: true,
+    component: AddTask,
+  },
+  {
+    link: "/tasks",
+    icon: faListCheck,
+    name: "My tasks",
+    modal: false,
+  },
+  {
+    link: "/projects",
+    icon: faProjectDiagram,
+    name: "My projects",
+    modal: false,
+  },
+  {
+    link: "/team",
+    icon: faUsersRectangle,
+    name: "Team members",
+    modal: false,
+  },
+];
 
 export const systemLinks = [
   {
@@ -16,36 +52,21 @@ export const systemLinks = [
     modal: false,
   },
   {
+    link: "/profile",
+    icon: faUser,
+    name: "My profile",
+    modal: false,
+  },
+  {
+    link: "/security",
+    icon: faShield,
+    name: "Security",
+    modal: false,
+  },
+  {
     link: "/settings",
     icon: faSliders,
     name: "Settings",
-    modal: false,
-  },
-];
-
-export const taskLinks = [
-  {
-    link: "/",
-    icon: faCubesStacked,
-    name: "Task overview",
-    modal: false,
-  },
-  {
-    link: "/tasks",
-    icon: faListCheck,
-    name: "View all tasks",
-    modal: false,
-  },
-  {
-    icon: faCalendarPlus,
-    name: "Add new task",
-    modal: true,
-    component: AddTask,
-  },
-  {
-    link: "/completed",
-    icon: faCalendarCheck,
-    name: "Completed tasks",
     modal: false,
   },
 ];
