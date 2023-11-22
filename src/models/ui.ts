1f75b0eee52e0ead0bf46e26e15758adab603e4e
amt-charles-biney <charles.biney@amalitech.com>
2023-11-22 08:52:03 +0000

    refactor: document and change openModal to OpenModal

--- src/models/ui.ts
+++ src/models/ui.ts
@@ -1,24 +1,39 @@
-import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
+/**
+ * Represents the props required for a modal component.
+ */
 export type ModalComponentProps = {
   closeModal: (isOpen: boolean) => void;
 };
 
+/**
+ * Represents the props required for a modal.
+ */
 export type ModalProps = {
   closeModal: (isOpen: boolean) => void;
   component: (props: ModalComponentProps) => JSX.Element;
 };
 
+/**
+ * Represents the state of a modal.
+ */
 export type ModalState = {
   isModalOpen: boolean;
   component?: (props: ModalComponentProps) => JSX.Element;
 };
 
+/**
+ * Function type for opening a modal.
+ */
 export type OpenModalFunction = (
   isOpen: boolean,
   component: (props: ModalComponentProps) => JSX.Element
 ) => void;
 
+/**
+ * Represents the props required for a QatButton component.
+ */
 export type QatButtonProps = {
   title: string;
   color: string;
@@ -27,14 +42,22 @@ export type QatButtonProps = {
   component?: (props: ModalComponentProps) => JSX.Element;
 };
 
-export type openModal = {
+/**
+ * Represents options for opening a modal.
+ */
+export type OpenModal = {
+  isMini?: boolean;
   openModal?: OpenModalFunction;
 };
 
+/**
+ * Represents the props required for a navigation component.
+ */
 export type NavProps = {
   link?: string;
   icon: IconDefinition;
   name: string;
+  isMini: boolean;
   openModal?: OpenModalFunction;
   component?: (props: ModalComponentProps) => JSX.Element;
 };
