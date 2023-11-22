61f94e21925b1f77c81a79c4ad0143a59ac04c83
amt-charles-biney <charles.biney@amalitech.com>
2023-11-22 08:21:08 +0000

    feat: add push-notification services

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AllTasks from "./components/outlets/allTasks";
+import Overview from "./components/outlets/overview";
 import Dashboard from "./pages/dashboard";
 import Login from "./pages/login";
 import { useAppSelector } from "./redux/hooks";
-import Overview from "./components/outlets/overview";
-import { Toaster } from "react-hot-toast";
-import AllTasks from "./components/outlets/allTasks";
+import runOneSignal from "./utilities/oneSignal";
 
 function App() {
-  const { isAuthenticated } = useAppSelector((state) => state.auth);
+  const { isAuthenticated, user } = useAppSelector((state) => state.auth);
+
+  useEffect(() => {
+    user && runOneSignal(user.id);
+  }, [user]);
+
   const root = createBrowserRouter([
     {
       path: "/",
@@ -28,6 +35,7 @@ function App() {
       ],
     },
   ]);
+
   return (
     <>
       <RouterProvider router={root} />
